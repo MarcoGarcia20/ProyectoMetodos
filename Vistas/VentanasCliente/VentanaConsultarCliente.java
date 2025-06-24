dd787dc451c9adb7a03fd0f5171d0b46907087c7
MarcoGarcia20 <josu1901456@gmail.com>
2025-06-24 13:13:04 -0500

    Update VentanaConsultarCliente.java

--- Vistas/VentanasCliente/VentanaConsultarCliente.java
+++ Vistas/VentanasCliente/VentanaConsultarCliente.java
@@ -1,17 +1,4 @@
 package Vistas.VentanasCliente;
-import Entidades.Cliente;
-import Persistencia.ClienteRepositorio;
-
-import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableColumnModel;
-
-import Controladores.ClienteControlador;
-
-import java.awt.BorderLayout;
-import java.awt.event.*;
-import java.io.*;
-import java.util.HashSet;
 
 public class VentanaConsultarCliente extends VentanaFormularioCliente{
 
@@ -21,7 +8,7 @@ public class VentanaConsultarCliente extends VentanaFormularioCliente{
 
     @Override
     protected void configurar() {
-        
+        //
     }
 }
 
